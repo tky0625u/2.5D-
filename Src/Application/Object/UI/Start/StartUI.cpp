243684b29d43ae4c05a1a896c8377a9c5626ce3b
tky0625u <kd1370302@st.kobedenshi.ac.jp>
2024-06-19 23:41:37 +0900

    StartUI
    GO

--- Src/Application/Object/UI/Start/StartUI.cpp
+++ Src/Application/Object/UI/Start/StartUI.cpp
@@ -18,7 +18,11 @@ void StartUI::Update()
 	}
 	else
 	{
-		if (m_size > StartSIZEMAX)m_size += 0.05f;
+		if (m_size > StartSIZEMAX)
+		{
+			m_size += Start_GO_SizeUP;
+			m_alpha -= StartAlphaDOWN;
+		}
 		else{m_size += StartSIZECHANGE;}
 	}
 
@@ -30,17 +34,22 @@ void StartUI::Update()
 		if (m_cutX == m_rect.width * Start_GO)
 		{
 			if(!m_StartFlg)m_StartFlg = true;
-			else { m_isExpired = true; }
 		}
 	}
 	m_rect = { long(m_cutX),0,m_rect.width,m_rect.height };
+	m_color = { 1.0f,1.0f,1.0f,m_alpha };
+
+	if (m_alpha < 0.0f)
+	{
+		m_isExpired = true;
+	}
 
 	m_frame++;
 }
 
 void StartUI::DrawSprite()
 {
-	KdShaderManager::Instance().m_spriteShader.DrawTex(&m_Tex, (int)m_pos.x, (int)m_pos.y, float(m_rect.width * m_size), float(m_rect.height * m_size), &m_rect);
+	KdShaderManager::Instance().m_spriteShader.DrawTex(&m_Tex, (int)m_pos.x, (int)m_pos.y, int(m_rect.width * m_size), int(m_rect.height * m_size), &m_rect, &m_color);
 }
 
 void StartUI::Init()
@@ -48,6 +57,8 @@ void StartUI::Init()
 	m_pos = {};
 	m_cutX = 0;
 	m_rect = { long(m_cutX),0,134,100 };
+	m_alpha = 1.0f;
+	m_color = { 1.0f,1.0f,1.0f,m_alpha };
 	m_size = 0.0f;
 	m_frame = 0.0f;
 	m_StartFlg = false;
--- Src/Application/Object/UI/Start/StartUI.h
+++ Src/Application/Object/UI/Start/StartUI.h
@@ -2,7 +2,9 @@
 
 #define StartSIZECHANGE 0.4f
 #define StartSIZEMAX    3.0f
-#define Start_GO        3        
+#define Start_GO        3
+#define Start_GO_SizeUP 0.05f
+#define StartAlphaDOWN  0.02f
 
 class StartUI :public KdGameObject
 {
@@ -19,6 +21,8 @@ public:
 private:
 	Math::Vector2   m_pos;
 	Math::Rectangle m_rect;
+	Math::Color     m_color;
+	float           m_alpha;
 	float           m_cutX;
 	float           m_size;
 	float           m_frame;
